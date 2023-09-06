5d04b24b42972361bdd3c5eabbc8b46bf83aee0e
BVNguyenAn <anbuiuytin@gmail.com>
2023-09-06 18:45:47 +0700

    done vangogh slider

---
+++ asset/js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.contain-boxes').slick({
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        slidesToShow: 2,
+        autoplaySpeed: 2000,
+        slidesToScroll: 2
+    });
+  });
